ae1c585c32a370b39ba24e7e8a0b0ffa363ee3ba
Victor Escorcia <escorciav@gmail.com>
2016-11-02 15:04:35 +0300

    [ncp] Create ncp package
    Module model allocate functions to create our neural-context-pooling
    model

---
+++ ncp/model.py
@@ -0,0 +1,65 @@
+from keras.layers import Input, Conv1D, Dense, Flatten, MaxPooling1D
+from keras.models import Model, Sequential
+
+
+def neural_context_encoder(arch_prm, input_shape):
+    """Modular architecture to encode context feature
+
+    Parameters
+    ----------
+    arch_prm : dict
+
+    """
+    first_layer = True
+    init_shape = dict(input_shape=input_shape)
+    combo_list, after_combo = arch_prm['combo_list'], arch_prm['after_combo']
+
+    nce_md = Sequential()
+    if arch_prm['type'] == 'conv':
+        for i, combo in enumerate(combo_list):
+            for j, c_unit in enumerate(combo):
+                c_unit_name = 'conv{}_{}'.format(i, j)
+                num_units, filter_size = 16, 3
+                if 'num_units' in c_unit:
+                    num_units = c_unit['num_units']
+                if 'filter_size' in c_unit:
+                    filter_size = c_unit['filter_size']
+
+                nce_md.add(Conv1D(num_units, filter_size, name=c_unit_name,
+                                  border_mode='same', activation='relu',
+                                  **init_shape))
+                if first_layer:
+                    init_shape = {}
+                    first_layer = False
+
+            if 'max-pool' in after_combo[i]:
+                pooling_prm = after_combo[i]['max-pool']
+                nce_md.add(MaxPooling1D(**pooling_prm))
+            if 'flatten' in after_combo[i]:
+                nce_md.add(Flatten())
+    else:
+        raise ValueError('Invalid type in arch_prm. Set it to "conv".')
+
+    return nce_md
+
+
+def neural_context_model(num_actions, receptive_field, arch_prm):
+    """Instanciate Keras model
+
+    Parameters
+    ----------
+    num_actions : int
+    receptive_field : tuple
+
+    """
+    input_src = Input(shape=receptive_field, name='context_over_time')
+    model = neural_context_encoder(arch_prm, receptive_field)
+    encoded_input = model(input_src)
+    output_prob = Dense(num_actions, activation='sigmoid',
+                        name='output_prob')(encoded_input)
+    output_offsets = Dense(2 * num_actions,
+                           name='output_offsets')(encoded_input)
+
+    model = Model(input=[input_src],
+                  output=[output_prob, output_offsets])
+    return model
